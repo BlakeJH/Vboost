448c1bbc332100ce68b2d7bcf07ba4328ef5e5cd
BlakeJH <blakejenkinshanks@gmail.com>
2023-03-05 18:08:17 -0700

    Changes

--- src/main/java/blake_jh/vboost/Vboost.java
+++ src/main/java/blake_jh/vboost/Vboost.java
@@ -1,5 +1,6 @@
 package blake_jh.vboost;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vboost extends JavaPlugin {
@@ -7,11 +8,14 @@ public final class Vboost extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getLogger().info("Hello World");
+        // Remove once done coding.
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("Shutting Down");
     }
 }
