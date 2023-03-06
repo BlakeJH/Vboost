a145494afc1c2aa99f53221d050a6520dbda7925
BlakeJH <blakejenkinshanks@gmail.com>
2023-03-05 18:04:38 -0700

    I created a github.

---
+++ src/main/java/blake_jh/vboost/Vboost.java
@@ -0,0 +1,17 @@
+package blake_jh.vboost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Vboost extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
